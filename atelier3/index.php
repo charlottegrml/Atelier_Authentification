1c587bf01435aab0dbdbf369a2249e27a5c6c4f8
charlottegrml <charlotte.gourmelond@gmail.com>
2025-11-05 11:32:48 +0100

    Update index.php

--- atelier3/index.php
+++ atelier3/index.php
@@ -22,7 +22,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Rediriger vers la page protégée
         header('Location: page_admin.php');
         exit();
-    } else {
+    } se if ($username === 'admin' && $password === 'secret') {
+        $jeton = bin2hex(random_bytes(16));
+        setcookie('authToken', $jeton, time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
+        header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
+        exit();
+    }
+    else {
         $error = "Nom d'utilisateur ou mot de passe incorrect.";
     }
 }
