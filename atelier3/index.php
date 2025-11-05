bc297f755a65cab9368766919b71480c28e69706
charlottegrml <charlotte.gourmelond@gmail.com>
2025-11-05 11:54:42 +0100

    Update index.php

--- atelier3/index.php
+++ atelier3/index.php
@@ -3,10 +3,7 @@
 session_start();
 
 // Vérifier si l'utilisateur est déjà connecté
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
-    header('Location: page_admin.php'); // Si l'utilisateur s'est déjà connecté alors il sera automatiquement redirigé vers la page protected.php
-    exit();
-}
+
 
 // Gérer le formulaire de connexion
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
