ce1df81b42e968b7ed476d8a143a7a9c799a9bbb
charlottegrml <charlotte.gourmelond@gmail.com>
2025-11-05 12:16:30 +0100

    Update page_user.php

--- atelier3/page_user.php
+++ atelier3/page_user.php
@@ -2,8 +2,8 @@
 // Démarrer la session
 session_start();
 
-$_SESSION['nbVisiteUser']++;
-$nbVisite = $_SESSION['nbVisiteUser'];
+//$_SESSION['nbVisiteUser']++;
+//$nbVisite = $_SESSION['nbVisiteUser'];
 // Vérifier si l'utilisateur s'est bienconnecté
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     header('Location: index.php'); // Dans le cas contraire, l'utilisateur sera redirigé vers la page de connexion
@@ -21,7 +21,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 <body>
     <h1>Bienvenue sur la page utilisateur de l'atelier 3</h1>
     <p>Vous êtes connecté en tant que : <?php echo htmlspecialchars($_SESSION['username']); ?></p>
-    <p>Nombre de Visite(s) en tant qu'Utilisateur : <?php echo $nbVisite; ?> </p>
+   
     <a href="logout.php">Se déconnecter</a>
 </body>
 </html>
